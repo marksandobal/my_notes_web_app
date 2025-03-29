5299e6c4bca59b8bbee464056063660d75f07e00
Arturo Hernandez <marksandobal@gmail.com>
2025-03-28 19:00:07 -0600

    Add formik and yuo validator forms

--- src/components/login/Form.tsx
+++ src/components/login/Form.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 import { signIn } from 'next-auth/react';
+import * as Yup from 'yup';
+import { Formik, Field, Form } from 'formik';
 
 import {
   Button,
@@ -10,85 +12,46 @@ import {
   Paper,
   Typography,
   Box,
-  ThemeProvider,
-  createTheme,
   CircularProgress,
 } from '@mui/material';
 
-interface IAuthErrors {
-  email?: string;
-  password?: string;
+interface ICredential {
+  email: string;
+  password: string;
 };
 
-interface ICredential {
-  email?: string;
-  password?: string;
-};
+export const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('Por favor ingrese un email válido')
+    .required('El email es obligatorio'),
+  password: Yup.string()
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    .required('La contraseña es obligatoria'),
+});
 
 export default function LoginForm() {
   const router = useRouter();
 
   const [isClient, setIsClient] = useState(false)
   const [isLoading, setIsLoading] = useState(false);
-  const [formData, setFormData] = useState<ICredential>({
-    email: '',
-    password: ''
-  });
-  const [errors, setErrors] = useState<IAuthErrors>({});
 
   useEffect(() => {
     setIsClient(true)
   }, [])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      [name]: '',
-    }));
-  };
-
-  const validateForm = () => {
-    const newErrors: IAuthErrors = {};
-
-    if (!formData.email || !formData.email.trim()) {
-      newErrors.email = 'Email is required';
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = 'Invalid email format';
-    }
-
-    if (!formData.password || !formData.password.trim()) {
-      newErrors.password = 'Password is required';
-    } else if (formData.password.length < 6) {
-      newErrors.password = 'Password must be at least 6 characters long';
-    }
-
-    setErrors(newErrors);
-
-    return Object.keys(newErrors).length === 0;
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (values: ICredential) => {
     setIsLoading(true);
-    e.preventDefault();
-    if (validateForm()) {
-      const result = await signIn('credentials', {
-        redirect: false,  // No redirigir automáticamente
-        email: formData.email,
-        password: formData.password,
-      });
-  
-      if (result?.error) {
-        console.error(result);
-        setIsLoading(false);
-      } else {
-        // Redirige al usuario después de un inicio de sesión exitoso
-        router.push('/');
-      }
+    const result = await signIn('credentials', {
+      redirect: false,
+      email: values.email,
+      password: values.password,
+    });
+
+    if (result?.error) {
+      console.error(result);
+      setIsLoading(false);
+    } else {
+      router.push('/');
     }
     setIsLoading(false);
   };
@@ -126,59 +89,71 @@ export default function LoginForm() {
       <Typography color='info'>
         para continuar con tu experiencia
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-          value={formData.email}
-          onChange={handleChange}
-          error={!!errors.email}
-          helperText={errors.email}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={formData.password}
-          onChange={handleChange}
-          error={!!errors.password}
-          helperText={errors.password}
-        />
-        <Box sx={{ m: 1, position: 'relative' }}>
-          <Button
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            type='submit'
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
           >
-            Iniciar sesión
-          </Button>
-          {isLoading && (
-            <CircularProgress
-              size={24}
-              sx={{
-                color: 'success',
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                marginTop: '-12px',
-                marginLeft: '-12px',
-              }}
-            />
+          {({ isSubmitting, errors, touched, handleChange, handleBlur }) => (
+            <Form>
+              <Box component="div" sx={{ mt: 1, width: '100%' }}>
+              <Field
+                  name="email"
+                  as={TextField}
+                  label="Email Address"
+                  id="email"
+                  fullWidth
+                  margin="normal"
+                  required
+                  autoComplete="email"
+                  autoFocus
+                  error={touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Field
+                  name="password"
+                  as={TextField}
+                  label="Password"
+                  type="password"
+                  id="password"
+                  fullWidth
+                  margin="normal"
+                  required
+                  autoComplete="current-password"
+                  error={touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Box sx={{ m: 1, position: 'relative' }}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    type='submit'
+                  >
+                    Iniciar sesión
+                  </Button>
+                  {isLoading && (
+                    <CircularProgress
+                      size={24}
+                      sx={{
+                        color: 'success',
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        marginTop: '-12px',
+                        marginLeft: '-12px',
+                      }}
+                    />
+                  )}
+                </Box>
+              </Box>
+            </Form>
           )}
-        </Box>
-      </Box>
+        </Formik>
     </Paper>
   </Box>
   );
